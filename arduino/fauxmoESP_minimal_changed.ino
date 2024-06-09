0480756cd8c200fd7f343d151a724a5ca0a3cf35
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-06-09 12:28:01 +0200

    Update fauxmoESP_minimal_changed.ino

--- arduino/fauxmoESP_minimal_changed.ino
+++ arduino/fauxmoESP_minimal_changed.ino
@@ -14,15 +14,9 @@ fauxmoESP fauxmo;
 #define LED                 4
 #define GPIO_RX             5
 #define GPIO_TX             0
-#define LED_PINK            2
-#define LED_WHITE           15
-
 #define ID_LED              "pin4"
 #define ID_GPIORX           "pin3"
 #define ID_GPIOTX           "pin1"
-#define ID_PINK             "pink lamp"
-#define ID_WHITE            "white lamp"
-
 
 
 void wifiSetup() {
@@ -69,27 +63,16 @@ void setup() {
     
     fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
         
-        // Callback when a command from Alexa is received. 
-        // You can use device_id or device_name to choose the element to perform an action onto (relay, LED,...)
-        // State is a boolean (ON/OFF) and value a number from 0 to 255 (if you say "set kitchen light to 50%" you will receive a 128 here).
-        // Just remember not to delay too much here, this is a callback, exit as soon as possible.
-        // If you have to do something more involved here set a flag and process it in your main loop.
         
         Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
 
-        // Checking for device_id is simpler if you are certain about the order they are loaded and it does not change.
-        // Otherwise comparing the device_name is safer.
-
-        if (strcmp(device_name, ID_YELLOW)==0) {
-            digitalWrite(LED_YELLOW, state ? HIGH : LOW);
-        } else if (strcmp(device_name, ID_GREEN)==0) {
-            digitalWrite(LED_GREEN, state ? HIGH : LOW);
-        } else if (strcmp(device_name, ID_BLUE)==0) {
-            digitalWrite(LED_BLUE, state ? HIGH : LOW);
-        } else if (strcmp(device_name, ID_PINK)==0) {
-            digitalWrite(LED_PINK, state ? HIGH : LOW);
-        } else if (strcmp(device_name, ID_WHITE)==0) {
-            digitalWrite(LED_WHITE, state ? HIGH : LOW);
+        
+        if (strcmp(device_name, ID_LED)==0) {
+            digitalWrite(LED, state ? HIGH : LOW);
+        } else if (strcmp(device_name, ID_GPIORX)==0) {
+            digitalWrite(GPIO_RX, state ? HIGH : LOW);
+        } else if (strcmp(device_name, ID_GPIOTX)==0) {
+            digitalWrite(GPIO_TX, state ? HIGH : LOW);
         }
 
     });
