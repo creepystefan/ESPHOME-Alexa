55dde84debcca4e831b6f3ba7ae4f55e5164a373
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-06-09 12:23:34 +0200

    Update fauxmoESP_minimal_changed.ino

--- arduino/fauxmoESP_minimal_changed.ino
+++ arduino/fauxmoESP_minimal_changed.ino
@@ -5,44 +5,32 @@
     #include <ESP8266WiFi.h>
 #endif
 #include "fauxmoESP.h"
-
-// Rename the credentials.sample.h file to credentials.h and 
-// edit it according to your router configuration
 #include "credentials.h"
 
 fauxmoESP fauxmo;
 
-// -----------------------------------------------------------------------------
-
 #define SERIAL_BAUDRATE     115200
 
-#define LED_YELLOW          4
-#define LED_GREEN           5
-#define LED_BLUE            0
+#define LED                 4
+#define GPIO_RX             5
+#define GPIO_TX             0
 #define LED_PINK            2
 #define LED_WHITE           15
 
-#define ID_YELLOW           "yellow lamp"
-#define ID_GREEN            "green lamp"
-#define ID_BLUE             "blue lamp"
+#define ID_LED              "pin4"
+#define ID_GPIORX           "pin3"
+#define ID_GPIOTX           "pin1"
 #define ID_PINK             "pink lamp"
 #define ID_WHITE            "white lamp"
 
-// -----------------------------------------------------------------------------
 
-// -----------------------------------------------------------------------------
-// Wifi
-// -----------------------------------------------------------------------------
 
 void wifiSetup() {
-
     // Set WIFI module to STA mode
     WiFi.mode(WIFI_STA);
-
     // Connect
     Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
-
     // Wait
     while (WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
@@ -66,40 +54,19 @@ void setup() {
     pinMode(LED_YELLOW, OUTPUT);
     pinMode(LED_GREEN, OUTPUT);
     pinMode(LED_BLUE, OUTPUT);
-    pinMode(LED_PINK, OUTPUT);
-    pinMode(LED_WHITE, OUTPUT);
-    digitalWrite(LED_YELLOW, LOW);
-    digitalWrite(LED_GREEN, LOW);
-    digitalWrite(LED_BLUE, LOW);
-    digitalWrite(LED_PINK, LOW);
-    digitalWrite(LED_WHITE, LOW);
-
+    
     // Wifi
     wifiSetup();
-
-    // By default, fauxmoESP creates it's own webserver on the defined port
-    // The TCP port must be 80 for gen3 devices (default is 1901)
-    // This has to be done before the call to enable()
     fauxmo.createServer(true); // not needed, this is the default value
     fauxmo.setPort(80); // This is required for gen3 devices
-
-    // You have to call enable(true) once you have a WiFi connection
-    // You can enable or disable the library at any moment
-    // Disabling it will prevent the devices from being discovered and switched
     fauxmo.enable(true);
 
-    // You can use different ways to invoke alexa to modify the devices state:
-    // "Alexa, turn yellow lamp on"
-    // "Alexa, turn on yellow lamp
-    // "Alexa, set yellow lamp to fifty" (50 means 50% of brightness, note, this example does not use this functionality)
-
     // Add virtual devices
-    fauxmo.addDevice(ID_YELLOW);
-    fauxmo.addDevice(ID_GREEN);
-    fauxmo.addDevice(ID_BLUE);
-    fauxmo.addDevice(ID_PINK);
-    fauxmo.addDevice(ID_WHITE);
-
+    fauxmo.addDevice(ID_);
+    fauxmo.addDevice(ID_LED);
+    fauxmo.addDevice(ID_GPIORX);
+    fauxmo.addDevice(ID_GPIOTX);
+    
     fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
         
         // Callback when a command from Alexa is received. 
@@ -130,21 +97,11 @@ void setup() {
 }
 
 void loop() {
-
-    // fauxmoESP uses an async TCP server but a sync UDP server
-    // Therefore, we have to manually poll for UDP packets
     fauxmo.handle();
 
-    // This is a sample code to output free heap every 5 seconds
-    // This is a cheap way to detect memory leaks
     static unsigned long last = millis();
     if (millis() - last > 5000) {
         last = millis();
         Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
     }
-
-    // If your device state is changed by any other means (MQTT, physical button,...)
-    // you can instruct the library to report the new state to Alexa on next request:
-    // fauxmo.setState(ID_YELLOW, true, 255);
-
 }
