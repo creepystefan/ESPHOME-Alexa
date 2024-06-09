db8bcc0635228e6a14d8d49393284ba4ec227482
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-06-09 14:51:18 +0200

    Update fauxmoESP_minimal_changed.ino

--- arduino/fauxmoESP_minimal_changed.ino
+++ arduino/fauxmoESP_minimal_changed.ino
@@ -44,22 +44,22 @@ void setup() {
     Serial.println();
     Serial.println();
 
-    // LEDs
-    pinMode(LED, OUTPUT);
-    pinMode(GPIO_RX, OUTPUT);
-    pinMode(GPIO_TX, OUTPUT);
-    
-    // Wifi
+   // Wifi
     wifiSetup();
     fauxmo.createServer(true); // not needed, this is the default value
     fauxmo.setPort(80); // This is required for gen3 devices
     fauxmo.enable(true);
+    
+    // LEDs
+    pinMode(LED, OUTPUT);         //#define LED        4
+    pinMode(GPIO_RX, OUTPUT);     //#define GPIO_RX    5
+    pinMode(GPIO_TX, OUTPUT);     //#define GPIO_TX    1
 
     // Add virtual devices
     fauxmo.addDevice(ID_);        
-    fauxmo.addDevice(ID_LED);     //#define ID_LED       "pin4"
-    fauxmo.addDevice(ID_GPIORX);  //#define ID_GPIORX    "pin3"
-    fauxmo.addDevice(ID_GPIOTX);  //#define ID_GPIORX    "pin1"
+    fauxmo.addDevice(ID_LED);      //#define ID_LED       "pin4"
+    fauxmo.addDevice(ID_GPIORX);   //#define ID_GPIORX    "pin3"
+    fauxmo.addDevice(ID_GPIOTX);   //#define ID_GPIORX    "pin1"
     
     fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
         
@@ -71,7 +71,7 @@ void setup() {
             digitalWrite(LED, state ? HIGH : LOW);          //#define LED              4
         } else if (strcmp(device_name, ID_GPIORX)==0) {     //#define ID_GPIORX    "pin3"
             digitalWrite(GPIO_RX, state ? HIGH : LOW);      //#define GPIO_RX             5
-        } else if (strcmp(device_name, ID_GPIOTX)==0) {     //#define ID_GPIORX    "pin1"
+        } else if (strcmp(device_name, ID_GPIOTX)==0) {     //#define ID_GPIOTX    "pin1"
             digitalWrite(GPIO_TX, state ? HIGH : LOW);      //#define GPIO_TX             1
         }
 
