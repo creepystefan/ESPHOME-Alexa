5d05f9e7191a6b4cb622aef8683d425184dc8f1b
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-06-16 18:15:59 +0200

    Update test.h

--- test.h
+++ test.h
@@ -45,13 +45,11 @@ class ALEXAESPtest : public Component {
 
   }
   void loop() override {
-    // This will be called very often after setup time.
-    // think of it as the loop() call in Arduino
-    if (digitalRead(5)) {
-      digitalWrite(6, HIGH);
+      fauxmo.handle();
 
-      // You can also log messages
-      ESP_LOGD("custom", "The GPIO pin 5 is HIGH!");
+    static unsigned long last = millis();
+    if (millis() - last > 5000) {
+        last = millis();
+        Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
     }
-  }
 };
