0999cc96002fa294a9d8418047948048b5f8c81b
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-06-16 18:28:52 +0200

    Update test.h

--- test.h
+++ test.h
@@ -5,10 +5,10 @@ fauxmoESP fauxmo;
 
 #define SERIAL_BAUDRATE     115200
 
-#define LED                 4
-#define GPIO_RX             5
+#define LED                 2
+#define GPIO_RX             3
 #define GPIO_TX             1
-#define ID_LED              "pin4"
+#define ID_LED              "pin2"
 #define ID_GPIORX           "pin3"
 #define ID_GPIOTX           "pin1"
 
@@ -28,13 +28,13 @@ class ALEXAESPtest : public Component {
     fauxmo.enable(true);
     
     // LEDs
-    pinMode(LED, OUTPUT);         //#define LED        4
-    pinMode(GPIO_RX, OUTPUT);     //#define GPIO_RX    5
+    pinMode(LED, OUTPUT);         //#define LED        2
+    pinMode(GPIO_RX, OUTPUT);     //#define GPIO_RX    3
     pinMode(GPIO_TX, OUTPUT);     //#define GPIO_TX    1
 
     // Add virtual devices
     fauxmo.addDevice(ID_);        
-    fauxmo.addDevice(ID_LED);      //#define ID_LED       "pin4"
+    fauxmo.addDevice(ID_LED);      //#define ID_LED       "pin2"
     fauxmo.addDevice(ID_GPIORX);   //#define ID_GPIORX    "pin3"
     fauxmo.addDevice(ID_GPIOTX);   //#define ID_GPIORX    "pin1"
     
@@ -44,8 +44,8 @@ class ALEXAESPtest : public Component {
         Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
 
 
-        if (strcmp(device_name, ID_LED)==0) {               //#define ID_LED       "pin4"
-            digitalWrite(LED, state ? HIGH : LOW);          //#define LED              4
+        if (strcmp(device_name, ID_LED)==0) {               //#define ID_LED       "pin2"
+            digitalWrite(LED, state ? HIGH : LOW);          //#define LED              2
         } else if (strcmp(device_name, ID_GPIORX)==0) {     //#define ID_GPIORX    "pin3"
             digitalWrite(GPIO_RX, state ? HIGH : LOW);      //#define GPIO_RX             5
         } else if (strcmp(device_name, ID_GPIOTX)==0) {     //#define ID_GPIOTX    "pin1"
