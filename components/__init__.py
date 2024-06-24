a686123df0bb3e08bf5032295dc2f96624a914b3
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-06-24 08:23:00 +0200

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,1 +1,9 @@
+import esphome.config_validation as cv
 
+CONF_MY_REQUIRED_KEY = 'alexa'
+CONF_MY_OPTIONAL_KEY = 'my_optional_key'
+
+CONFIG_SCHEMA = cv.Schema({
+  cv.Required(CONF_MY_REQUIRED_KEY): cv.string,
+  cv.Optional(CONF_MY_OPTIONAL_KEY, default=10): cv.int_,
+}).extend(cv.COMPONENT_SCHEMA)
