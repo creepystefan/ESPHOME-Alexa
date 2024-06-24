d0c9962e7434f5b4477ac9bb0682cb49d8e588d3
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-06-24 09:19:55 +0200

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,50 +1,65 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import modbus
-from esphome.const import CONF_ID
+from esphome.components import switch
+from esphome.const import (
+    CONF_ENTITY_CATEGORY,
+    CONF_ICON,
+    CONF_ID,
+    CONF_OUTPUT,
+    ENTITY_CATEGORY_CONFIG,
+)
+
+from .. import CONF_DPS_ID, DPS_COMPONENT_SCHEMA, dps_ns
+
+DEPENDENCIES = ["dps"]
 
 CODEOWNERS = ["@syssi"]
 
-DEPENDENCIES = ["modbus"]
-AUTO_LOAD = ["binary_sensor", "number", "sensor", "switch", "text_sensor"]
-MULTI_CONF = True
+# CONF_OUTPUT from const
+CONF_KEY_LOCK = "key_lock"
 
-CONF_DPS_ID = "dps_id"
-CONF_CURRENT_RESOLUTION = "current_resolution"
+ICON_OUTPUT = "mdi:power"
+ICON_KEY_LOCK = "mdi:play-box-lock-outline"
 
-dps_ns = cg.esphome_ns.namespace("dps")
-Dps = dps_ns.class_("Dps", cg.PollingComponent, modbus.ModbusDevice)
-
-CurrentResolution = dps_ns.enum("CurrentResolution")
-CURRENT_RESOLUTION_OPTIONS = {
-    "AUTO": CurrentResolution.DPS_CURRENT_RESOLUTION_AUTO,
-    "LOW": CurrentResolution.DPS_CURRENT_RESOLUTION_LOW,
-    "HIGH": CurrentResolution.DPS_CURRENT_RESOLUTION_HIGH,
+SWITCHES = {
+    CONF_OUTPUT: 0x0009,
+    CONF_KEY_LOCK: 0x0006,
 }
 
-DPS_COMPONENT_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_DPS_ID): cv.use_id(Dps),
-    }
-)
+DpsSwitch = dps_ns.class_("DpsSwitch", switch.Switch, cg.Component)
 
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(Dps),
-            cv.Optional(CONF_CURRENT_RESOLUTION, default="AUTO"): cv.enum(
-                CURRENT_RESOLUTION_OPTIONS, upper=True
-            ),
-        }
-    )
-    .extend(cv.polling_component_schema("5s"))
-    .extend(modbus.modbus_device_schema(0x01))
+CONFIG_SCHEMA = DPS_COMPONENT_SCHEMA.extend(
+    {
+        cv.Optional(CONF_OUTPUT): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(DpsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_OUTPUT): cv.icon,
+                cv.Optional(
+                    CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_CONFIG
+                ): cv.entity_category,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),
+        cv.Optional(CONF_KEY_LOCK): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(DpsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_KEY_LOCK): cv.icon,
+                cv.Optional(
+                    CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_CONFIG
+                ): cv.entity_category,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),
+    }
 )
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await modbus.register_modbus_device(var, config)
-
-    cg.add(var.set_current_resolution(config[CONF_CURRENT_RESOLUTION]))
+    hub = await cg.get_variable(config[CONF_DPS_ID])
+    for key, address in SWITCHES.items():
+        if key in config:
+            conf = config[key]
+            var = cg.new_Pvariable(conf[CONF_ID])
+            await cg.register_component(var, conf)
+            await switch.register_switch(var, conf)
+            cg.add(getattr(hub, f"set_{key}_switch")(var))
+            cg.add(var.set_parent(hub))
+            cg.add(var.set_holding_register(address))
