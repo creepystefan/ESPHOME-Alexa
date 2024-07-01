16d603578b0c9f4349def958424ed01f26019215
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-07-01 23:32:44 +0200

    Update fauxmoESP.h

--- src/fauxmoESP.h
+++ src/fauxmoESP.h
@@ -38,11 +38,7 @@ THE SOFTWARE.
 	#error Platform not supported
 #endif
 
-#include <WiFiUdp.h>
-#include <functional>
-#include <vector>
-#include <MD5Builder.h>
-#include "templates.h"
+
 
 #pragma once
 
@@ -72,9 +68,10 @@ THE SOFTWARE.
 #define DEBUG_FAUXMO_VERBOSE_UDP    false
 #endif
 
+#include <WiFiUdp.h>
 #include <functional>
 #include <vector>
-//#include <MD5Builder.h>
+#include <MD5Builder.h>
 #include "templates.h"
 
 typedef std::function<void(unsigned char, const char *, bool, unsigned char)> TSetStateCallback;
