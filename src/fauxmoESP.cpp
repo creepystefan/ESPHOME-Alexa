1d48fd3f8edb16bcfb7b02f9de41027164166907
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-07-01 07:58:03 +0200

    Update fauxmoESP.cpp

--- src/fauxmoESP.cpp
+++ src/fauxmoESP.cpp
@@ -25,7 +25,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 */
-#include <ESPhome.h>
+#include "ESPhome.h"
 //#include <Arduino.h>
 #include "fauxmoESP.h"
 
