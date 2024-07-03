fb44f1ccfa52edf811ee16f04074b6196c6ee4a6
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-07-03 23:09:40 +0200

    Create alexa.h

---
+++ alexa.h
@@ -0,0 +1,39 @@
+#include "esphome.h"
+#include <weenyMo.h>
+//
+// This function will be called when Alexa hears "switch [on | off] 'vee three'" 
+// ESP8266 builtin LED is "backwards" i.e. active LOW, hence Alexa's ON=1 needs reversing
+//
+
+#define BUILTIN_LED          4
+//"vee three"
+#define DEVICENAME           LED
+
+void onVoiceCommand(bool onoff){ 
+  Serial.printf("onVoiceCommand %d\n",onoff);
+  digitalWrite(BUILTIN_LED,!onoff);
+} 
+//
+bool getAlexaState(){ // We need this to let Alexa know if we are on or off
+  return !digitalRead(BUILTIN_LED); // Its "active low" so reverse it
+}
+// The only constructor: const char* name, function<void(bool)> onCommand
+// choose the name wisely: Alexa has very selective hearing!
+//
+weenyMo w("DEVICENAME",onVoiceCommand);
+
+class MyCustomComponent : public Component {
+ public:
+
+void setup() {
+  Serial.begin(74880);
+  pinMode(BUILTIN_LED,OUTPUT);
+  digitalWrite(BUILTIN_LED,HIGH); // start OFF 
+  WiFi.begin("ssid","pass"); // replace OF COURSE
+  WiFi.waitForConnectResult();  // so much neater than those stupid loops and dots
+  w.gotIPAddress(); // ready to roll...Tell Alexa to discover devices.
+}
+
+void loop() override {}
+
+};
