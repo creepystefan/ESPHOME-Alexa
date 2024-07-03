1f4b00b9153c496b72960d5179bdeb03c3638db8
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-07-03 21:42:20 +0200

    Update weenymo.h

--- src/weenymo.h
+++ src/weenymo.h
@@ -21,11 +21,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+#include "esphome.h"
 #ifndef weenyMo_H
 #define weenyMo_H
 #include <ESPAsyncUDP.h>
 #include <ESPAsyncWebServer.h>
-#include "esphome.h"
 #include <functional>
 using namespace std;
 using namespace std::placeholders;
